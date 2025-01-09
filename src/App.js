f54c77a188655cf3e509ff5ad3e662871d312e69
Jeffrey-Ragland <jeffreyragland3009@gmail.com>
2025-01-09 14:09:33 +0530

    jef commit

--- src/App.js
+++ src/App.js
@@ -1,21 +1,20 @@
-import './App.css';
-import Sidebar from './components/Sidebar';
-import { Outlet } from 'react-router-dom';
+import "./App.css";
+import Sidebar from "./components/Sidebar";
+import { Outlet } from "react-router-dom";
 
-
+// jan 9
 function App() {
   return (
-      <div className="flex">
+    <div className="flex">
+      <div>
+        <Sidebar />
+      </div>
+      <div className="basis-[100%]">
         <div>
-          <Sidebar/>
-        </div>
-        <div className='basis-[100%]'>
-          <div>
-            <Outlet>
-            </Outlet>
-          </div>
+          <Outlet></Outlet>
         </div>
       </div>
+    </div>
   );
 }
 
